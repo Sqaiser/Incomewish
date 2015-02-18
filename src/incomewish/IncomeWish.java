38ab8edd58b255a2ec3029eeae3b6bf546c63a9d
class <class@CST_N417_16.cst.citytech.cuny.edu>
2015-02-18 17:37:08 -0500

    finished income wish

--- src/incomewish/IncomeWish.java
+++ src/incomewish/IncomeWish.java
@@ -16,7 +16,17 @@ public class IncomeWish {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+     String name = "Sameen Qaiser";
+     int age = 22;
+     double annualpay = 100000;
+     System.out.println("My name is " + name + ", my age is " + age + " and\n" +
+     "I hope to earn 10000 $" + annualpay + "per year");
+     
+     
+     
       
+              
+              
     }
     
 }
